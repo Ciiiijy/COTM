c1c547c35112feee34b3efc074fc661b3ab40e22
BessieC <bessiechen08@foxmail.com>
2025-02-10 23:03:58 +0800

    feat: new graphic effect.

--- Assets/Scripts/Scene/OnTheBus.cs
+++ Assets/Scripts/Scene/OnTheBus.cs
@@ -1,13 +1,26 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OnTheBus : MonoBehaviour
 {
+    public Transform bus;
+    public Transform character;
 
+
+    
     public void Init() 
     {
         Hide();
+    }
+
+    private void Anim_ShackBus() 
+    {
+        DOTween.Sequence()
+            .Append(bus.DOLocalMoveY(0.37f,0.2f))
+            .Append(bus.DOLocalMoveY(0f, 0.2f))
+            .SetLoops(-1,LoopType.Yoyo);
     }
 
     public void Show() { gameObject.SetActive(true); }
@@ -16,7 +29,7 @@ public class OnTheBus : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Anim_ShackBus();
     }
 
     // Update is called once per frame
